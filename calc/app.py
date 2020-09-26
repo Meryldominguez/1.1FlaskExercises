5ba3066773b5654a414f756f31eb14b8cf1194de
Meryl Dominguez <meryldominguez@gmail.com>
2020-09-25 20:26:37 -0500

    imported the unittesting and moved on to the functions in calc

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,33 @@
 # Put your app in here.
+from operations import add, sub, mult, div
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/add")
+def add_request():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a,b)
+    return str(result)
+
+@app.route("/sub")
+def sub_request():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a,b)
+    return str(result)
+
+@app.route("/mult")
+def mult_request():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = mult(a,b)
+    return str(result)
+
+@app.route("/div")
+def div_request():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a,b)
+    return str(result)
--- greet/test.py
+++ greet/test.py
@@ -1,4 +1,4 @@
-from app.py import app
+from app import app
 import unittest
 
 class TestSimpleRoutes(unittest.TestCase):
